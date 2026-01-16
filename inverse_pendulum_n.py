796a622ed7b236ecf09219a913d84bd31147e08e
Filip Becanovic <becaphilippe@gmail.com>
2026-01-16 11:07:29 +0100

    added command line functionality to inverse pendulum generation

--- inverse_pendulum_n.py
+++ inverse_pendulum_n.py
@@ -1,16 +1,17 @@
+import sys
+import string
 import xml.etree.ElementTree as ET
 
-def generate_pendulum_n(filename = 'pendulum_n.xml'):
-    mujoco = ET.Element("mujoco", model="pendulum_n")
+def generate_pendulum_n(pendulum_order=1, filename='inverted_pendulum_1.xml'):
+    mujoco = ET.Element("mujoco", model=filename.replace(".xml", ""))
     worldbody = ET.SubElement(mujoco, "worldbody")
     ET.SubElement(worldbody, "light", diffuse=".8 .8 .8", pos="0 0 3", dir="0 0 -1")
     ET.SubElement(worldbody, "geom", type="plane", size='1 1 .1', rgba=".8 .8 .8 1")
     #body0 = ET.SubElement(worldbody, 'body' , pos = '0 0 0')
     parent = worldbody
     #ET.SubElement(body0, 'joint', type = 'hinge', axis = '0 -1 0', pos = '0 0 0')
-    #ET.SubElement(body0, 'geom', type = 'box', pos = '0 0 .5', size = '.1 .1 .5', rgba = '.1 .8 .1 1')
-    n = int(input('unesite red inverznog klatna'))
-    for i in range(n):
+    #ET.SubElement(body0, 'geom', type = 'box', pos = '0 0 .5', size = '.1 .1 .5', rgba = '.1 .8 .1 1'))
+    for i in range(pendulum_order):
         if i == 0:
             parent = ET.SubElement(parent, 'body', pos = '0 0 0')
         else:
@@ -23,6 +24,24 @@ def generate_pendulum_n(filename = 'pendulum_n.xml'):
     ET.indent(tree, space="  ", level=0)
     tree.write(filename, encoding="utf-8", xml_declaration=True)
 
-generate_pendulum_n()
 
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Script takes in at least 1 argument.")
+        exit()
 
+    if len(sys.argv) > 1:
+        pendulum_order = int(sys.argv[1])
+        if pendulum_order < 1:
+            raise ValueError("pendulum_order must be >= 1.")
+
+    if len(sys.argv) > 2:
+        filename = sys.argv[2]
+        if not filename.endswith(".xml"):
+            filename = filename + ".xml"
+    else:
+        filename = f"inverted_pendulum_{pendulum_order}.xml"
+        
+    generate_pendulum_n(pendulum_order, filename)
+
+    pass
