9956f599f10c71cb96ea8957697914d95e21b1a5
mare422 <marko.tepavcevic02@gmail.com>
2026-02-06 02:57:18 +0100

    Dodat početni model, stvoren .xml fajl i test

--- testiranje.py
+++ testiranje.py
@@ -1,2 +1,10 @@
-# pokusaj push-a preko VS Code interfejsa
-print("Ovo je testiranje push-a na GitHub.")
+#pokretanje MuJoCo viewer-a za testiranje XML modela
+
+import mujoco
+import mujoco.viewer
+
+model = mujoco.MjModel.from_xml_path("simple_human.xml") # zamenite ime fajla s nazivom vašeg XML modela
+data = mujoco.MjData(model)
+
+# Pokretanje interaktivnog viewer-a
+mujoco.viewer.launch(model, data)
